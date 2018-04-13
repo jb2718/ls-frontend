b59da9553becdd71515e538b50bbb9b58d65cf28
jb2718 <jbattle2718@gmail.com>
2018-04-12 19:31:02 -0500

    fixing tab display issue

--- 210/logic_flow_control/rot13/rot13.js
+++ 210/logic_flow_control/rot13/rot13.js
@@ -43,4 +43,5 @@ console.log(rot13('Teachers open the door, but you must enter by yourself.'));
 
 
 console.log(rot13(rot13('Teachers open the door, but you must enter by yourself.')));
-// logs: Teachers open the door, but you must enter by yourself.
+// logs:
+// Teachers open the door, but you must enter by yourself.
